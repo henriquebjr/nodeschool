67cfe6d778458ffc6f4280fa3ffced7c0816ea8b
Henrique B Jr <henriquebjr@gmail.com>
2015-09-29 22:12:03 -0300

    finish learnyounode

--- learnyounode/13_httpjsonapiserver.js
+++ learnyounode/13_httpjsonapiserver.js
@@ -1,16 +1,25 @@
 var http = require('http'),
-  url = require('url'),
-  startsWith = require('string.prototype.startswith');
+  url = require('url');
 
 var server = http.createServer(function(req, res){
+  var path = url.parse(req.url, true);
   res.writeHead(200, { 'Content-Type': 'application/json' });
 
-  if(req.method == 'GET' && req.url.startsWith('/api/')){
-    if(req.url.startsWith('/api/parsetime?')){
-      console.log("parsetime....");
+  if(req.method == 'GET'){
+    if(path.pathname == '/api/parsetime'){
+      var date = new Date(path.query.iso);
+      var time = new Object();
+      time.hour = date.getHours();
+      time.minute = date.getMinutes();
+      time.second = date.getSeconds();
+      res.end(JSON.stringify(time));
     }
-    if(req.url.startsWith('/api/unixtime?')){
-      console.log("unixtime....");
+    if(path.pathname == '/api/unixtime'){
+      var unix = new Date(path.query.iso);
+      var unixTime = unix.getTime();
+      var time = new Object();
+      time.unixtime = unixTime;
+      res.end(JSON.stringify(time));
     }
   }
 });
